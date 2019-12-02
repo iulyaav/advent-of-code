6b2b19e1c206ac15da0acba826bb1c4af55f4de3
Iulia Avram <avram.iulia.augusta@gmail.com>
2019-12-02 19:06:26 +0100

    Add second problem

--- 2019/day_two/2.py
+++ 2019/day_two/2.py
@@ -1,10 +1,10 @@
 import sys
+import copy
 
-def intcode(data):
-    data = [int(x) for x in data.split(',')]
+def intcode(data, noun, verb):
     index = 0
-    data[1] = 12
-    data[2] = 2
+    data[1] = noun
+    data[2] = verb
 
     while data[index] != 99:
         if data[index] == 1:
@@ -13,10 +13,29 @@ def intcode(data):
             data[data[index + 3]] = data[data[index + 1]] * data[data[index + 2]]
 
         index += 4
+    print(data)
+
     return data[0]
 
 
 if __name__ == "__main__":
     data_file = sys.argv[1]
     with open(data_file) as f:
-        print(intcode(f.read()))
+        data = [int(x) for x in f.read().split(',')]
+        n = min(100, len(data))
+        found = None
+        results = []
+        for i in range(n):
+            for j in range(n):
+                # print("Trying {} and {}".format(i, j)) 
+                data_copy = copy.deepcopy(data)
+                try:
+                    result = intcode(data_copy, i, j)
+                    if result == 19690720:
+                        print("Evrika! Result is {}".format(i * 1000 + j))
+                        break
+                except IndexError:
+                    pass
+
+        
+
