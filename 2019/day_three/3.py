139d87633422b85fcb3ee7e6aad785d4f8d5ea16
jules <avram.iulia.augusta@gmail.com>
2019-12-04 12:55:36 +0100

    Solve first part of day 3

---
+++ 2019/day_three/3.py
@@ -0,0 +1,44 @@
+import sys
+
+INSTRUCTIONS = {
+        'R': lambda x, y: (x+1, y),
+        'L': lambda x, y: (x-1, y),
+        'U': lambda x, y: (x, y+1), 
+        'D': lambda x, y: (x, y-1)
+}
+
+def compute_location(path, also_check=None):
+    locations = [(0, 0)]
+    for step in path:
+        for i in range(int(step[1:])):
+            prev_step = locations[-1]
+            new_step = INSTRUCTIONS[step[0]](prev_step[0], prev_step[1])
+            locations.append(new_step)
+    
+    if also_check is not None:
+        return set(locations).intersection(set(also_check))
+    
+    return locations
+    
+def manhattan_distance(A, B):
+    return abs(A[0] - B[0]) + abs(A[1] - B[1])
+
+def crossed_wires(first, second):
+    first = first.split(',')
+    second = second.split(',')
+    starting_point = (0, 0)
+
+    first_result = compute_location(first)
+    shared_locations = compute_location(second, also_check=first_result)
+    max_thus_far = 10 ** 9
+    for point in shared_locations:
+        distance = manhattan_distance(A=starting_point, B=point)
+        if point != (0, 0) and distance < max_thus_far:
+            max_thus_far = distance
+    return max_thus_far
+
+if __name__ == "__main__":
+    data = None
+    with open(sys.argv[1]) as f:
+        first, second, _ = f.read().split('\n')
+    print(crossed_wires(first, second))
