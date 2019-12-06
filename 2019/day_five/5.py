a156112a06f4cbf6464f4cb0f5f03277980007fd
jules <avram.iulia.augusta@gmail.com>
2019-12-06 11:08:08 +0100

    Improve neatness of code for day 5

--- 2019/day_five/5.py
+++ 2019/day_five/5.py
@@ -1,57 +1,69 @@
 import sys
 import time
 
-def intcode(data, noun=None, verb=None):
+DATA = []
+
+def addition(index, param_1, param_2, param_3):
+    DATA[param_3] = DATA[param_1] + DATA[param_2]
+    return index + 4
+
+def multiplication(index, param_1, param_2, param_3):
+    DATA[param_3] = DATA[param_1] * DATA[param_2]
+    return index + 4
+
+def read(index, param):
+    input_value = int(input("Give me an input value: "))
+    DATA[param] = input_value
+    return index + 2
+
+def write(index, param):
+    print("OUTPUT at index {}: {}".format(index, DATA[param]))
+    return index + 2
+
+def get_parameters(start, steps, mode="000"):
+    if steps is None:
+        return
+
+    parameter_instructions = {
+            "0": lambda index: DATA[index],
+            "1": lambda index: index
+    }
+
+    for i in range(steps):
+        yield parameter_instructions[mode[-1 - i]](start+i)
+
+def intcode(noun=None, verb=None):
     index = 0
 
     if noun is not None:
-        data[1] = noun
+        DATA[1] = noun
     if verb is not None:
-        data[2] = verb
-    while data[index] % 100 != 99:
+        DATA[2] = verb
 
-        opcode = data[index] % 10
-        instructions = str(data[index] // 100)
+    operations = {
+            1: lambda index: addition(index, *get_parameters(index+1, steps=3, mode=instructions)),
+            2: lambda index: multiplication(index, *get_parameters(index+1, steps=3, mode=instructions)),
+            3: lambda index: read(index, next(get_parameters(index+1, steps=1, mode=instructions))),
+            4: lambda index: write(index, next(get_parameters(index+1, steps=1, mode=instructions))),
+    }
+
+    while True:
+
+        opcode = DATA[index] % 10
+        instructions = str(DATA[index] // 100)
 
         if len(instructions) < 3:
             instructions = "0" * (3 - len(instructions)) + instructions
-
-        if opcode == 1:
-            param_1 = data[data[index + 1]] if instructions[2] == "0" else data[index + 1]
-            param_2 = data[data[index + 2]] if instructions[1] == "0" else data[index + 2]
-            param_3 = data[index + 3] if instructions[0] == "0" else index + 3
-
-            data[param_3] = param_1 + param_2
-            index += 4
-
-        elif opcode == 2:
-            param_1 = data[data[index + 1]] if instructions[2] == "0" else data[index + 1]
-            param_2 = data[data[index + 2]] if instructions[1] == "0" else data[index + 2]
-            param_3 = data[index + 3] if instructions[0] == "0" else index + 3
-            
-            data[param_3] = param_1 * param_2
-            index += 4
-
-        elif opcode == 3:
-            print("Gimme value")
-            input_value = int(input())
-            param = data[index+1] if instructions[2] == "0" else index+1
-            data[param] = input_value
-            index += 2
-
-        elif opcode == 4:
-            param = data[index+1] if instructions[2] == "0" else index+1
-            print("OUTPUT at index {}: {}".format(index, data[param]))
-            index += 2
-        
-        else:
-            print("Something went wrong at index {} with value {}".format(index, data[index]))
+        try:
+            index = operations[opcode](index)
+        except:
+            print("Something went wrong at index {} with value {}".format(index, DATA[index]))
             break
 
-    return data[0]
+    return DATA[0]
 
 
 if __name__ == "__main__":
     with open(sys.argv[1]) as f:
-        data = [int(x) for x in f.read().split(',')]
-        print(intcode(data))
+        DATA = [int(x) for x in f.read().split(',')]
+        print(intcode())
